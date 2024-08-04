9749bb6a1836f38d694eb9c0e358a8e5c4cb152c
teklingl <teklingl@gmail.com>
2024-08-04 14:03:42 -0400

    Practice/Examples

---
+++ forloops.py
@@ -0,0 +1,23 @@
+# This program demonstrates various for-loops
+
+# Prints out the numbers 0,1,2,3,4
+for x in range(5):
+	print(x)
+
+print("======")   # Separator
+
+# Prints out 5, 10, 15, …, 100
+for x in range(5, 105, 5):
+	print(x)
+
+print("======")   # Separator
+
+# Prints out 3,4,5
+for x in range(3, 6):
+	print(x)
+
+print("======")   # Separator
+
+# Prints out 10, 9, 8, …, 1
+for x in range(10, 0, -1):
+	print(x)
