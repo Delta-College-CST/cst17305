31ee9c30484594650296a49f42f05f07d1d9603a
teklingl <teklingl@gmail.com>
2024-08-05 09:04:24 -0400

    Practice/Examples

--- assigngrade2.py
+++ assigngrade2.py
@@ -2,7 +2,7 @@
 # if a student is passing or failing, and assigns
 # a letter grade
 
-# Input two numbers
+# Input three test scores
 test1 = int(input("Enter Test 1 score: "))
 test2 = int(input("Enter Test 2 score: "))
 test3 = int(input("Enter Test 3 score: "))
